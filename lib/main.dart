ef73ca43d18ba555096c373c8c735c881029a4d8
afluffysquirrel <afluffysquirrel@gmail.com>
2021-11-12 16:22:45 +0000

    v0.083

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,13 @@ import 'package:bouldr/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_launcher_icons/android.dart';
+import 'package:flutter_launcher_icons/constants.dart';
+import 'package:flutter_launcher_icons/custom_exceptions.dart';
+import 'package:flutter_launcher_icons/ios.dart';
+import 'package:flutter_launcher_icons/main.dart';
+import 'package:flutter_launcher_icons/utils.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
